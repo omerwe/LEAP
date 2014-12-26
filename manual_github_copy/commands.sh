de6ae4d36f44c3d70e14b38f83555d33f2d5902f
omerwe <omerw@cs.technion.ac.il>
2014-12-26 17:08:54 +0200

    Delete manual_copy

--- manual_github_copy/commands.sh
+++
@@ -1,7 +0,0 @@
-pip uninstall leap_gwas
-
-python setup.py sdist upload -r pypitest
-pip install --user  -i https://testpypi.python.org/pypi leap_gwas
-
-#python setup.py sdist upload -r pypi
-#pip install --user  -i https://pypi.python.org/pypi leap_gwas
